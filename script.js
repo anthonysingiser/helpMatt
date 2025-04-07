494fc0e574ecd36a38e2af728cd748975c868b6b
Mehasz <tup83552@temple.edu>
2025-04-07 10:14:25 -0400

    compressor almost working

--- script.js
+++ script.js
@@ -1,9 +1,38 @@
 // Create the Audio Context — this is the main control center for all audio operations
 const soundCtx = new AudioContext();
 
-// Create a GainNode — this will control the overall volume
-const masterGain = soundCtx.createGain();
-masterGain.gain.value = 1.0; // Set the master volume to full (1.0)
+//------------------------AUDIO DECODING--------------------
+let source;
+const loadPlayAudio = async function () {
+  const file = await fetch("snare_01.wav");
+  const arrayBuffer = await file.arrayBuffer();
+  const audioBuffer = await soundCtx.decodeAudioData(arrayBuffer);
+  source = soundCtx.createBufferSource();
+  source.buffer = audioBuffer;
+  source.start();
+};
 
-// Connect the master gain node to the audio output (speakers or headphones)
+const stopAudio = function () {
+  source.stop();
+};
+//---------------------------COMPRESSOR VALUES--------------
+let compressor = soundCtx.createDynamicsCompressor();
+compressor.threshold.setValueAtTime(-10, soundCtx.currentTime); // dB
+compressor.knee.setValueAtTime(10, soundCtx.currentTime); // dB
+compressor.ratio.setValueAtTime(6, soundCtx.currentTime); // ratio
+compressor.attack.setValueAtTime(0.05, soundCtx.currentTime); // sec
+compressor.release.setValueAtTime(0.25, soundCtx.currentTime);
+//--------------------------MASTER GAIN-----------------------
+let masterGain = soundCtx.createGain();
+masterGain.gain.value = 0.7;
+//--------------------------ROUTING---------------------------
+
+source.connect(compressor);
+compressor.connect(masterGain);
 masterGain.connect(soundCtx.destination);
+
+//--------------------------HTML CONNECT----------------------
+
+document.getElementById("start").addEventListener("click", loadPlayAudio);
+
+document.getElementById("stop").addEventListener("click", stopAudio);
